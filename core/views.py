0bd1dc15e56ba7a266bd66a24af9ac7c606af7c4
kydlaugan <kydlaugan71@gmail.com>
2023-09-03 02:13:41 +0100

    ajout du css

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from core.models import *
 
 # Create your views here.
+
+    
+class home(TemplateView):
+    template_name = "ytrasbook.html"
+    def get(self, request):
+        return render(request, self.template_name, None)
+    
